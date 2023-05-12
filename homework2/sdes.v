ae355e2722d26a868b0f06c3d955b90e65149848
Nathan Wiley <nathan.wiley@oit.edu>
2023-05-12 00:06:06 -0700

    Decryption added

--- homework2/sdes.v
+++ homework2/sdes.v
@@ -12,7 +12,7 @@ wire [7:0] key2out;
 wire [7:0] ip_out;
 wire [3:0] fk_a_out;
 wire [3:0] fk_b_out;
-wire [7:0] ciphertext;
+wire [7:0] text;
 //=======================================================
 //  Structural coding
 //=======================================================
@@ -25,17 +25,35 @@ keygen keyfn(.key10(inputkey[9:0]), .key1(key1out[7:0]), .key2(key2out[7:0]) );
 
 /* 		ENCRYPTION			*/
 //Perform first IP Permutation
+//ip(.in(SW[7:0]), .ipout(ip_out[7:0]) );
+//
+////Scary Black box of fk
+//fk fk_a(.a(ip_out[7:4]), .b(ip_out[3:0]), .key(key1out[7:0]), .out(fk_a_out[3:0]));
+//
+////Absolute magic
+//fk fk_b(.a(ip_out[3:0]), .b(fk_a_out[3:0]), .key(key2out[7:0]), .out(fk_b_out[3:0]));
+//
+////Inverse results
+//ip_inv inv(.in({fk_b_out[3:0],fk_a_out[3:0]}), .out(text[7:0]));
+//
+//assign LEDR[7:0] = text[7:0];
+/*		END ENCRYPTION			*/
+
+
+
+/* 		DECRYPTION			*/
+//Perform first IP Permutation
 ip(.in(SW[7:0]), .ipout(ip_out[7:0]) );
 
 //Scary Black box of fk
-fk fk_a(.a(ip_out[7:4]), .b(ip_out[3:0]), .key(key1out[7:0]), .out(fk_a_out[3:0]));
+fk fk_a(.a(ip_out[7:4]), .b(ip_out[3:0]), .key(key2out[7:0]), .out(fk_a_out[3:0]));
 
 //Absolute magic
-fk fk_b(.a(ip_out[3:0]), .b(fk_a_out[3:0]), .key(key2out[7:0]), .out(fk_b_out[3:0]));
+fk fk_b(.a(ip_out[3:0]), .b(fk_a_out[3:0]), .key(key1out[7:0]), .out(fk_b_out[3:0]));
 
 //Inverse results
-ip_inv inv(.in({fk_b_out[3:0],fk_a_out[3:0]}), .out(ciphertext[7:0]));
+ip_inv inv(.in({fk_b_out[3:0],fk_a_out[3:0]}), .out(text[7:0]));
 
-assign LEDR[7:0] = ciphertext[7:0];
-
+assign LEDR[7:0] = text[7:0];
+/*		END DECRYPTION			*/
 endmodule
